f7b80f1f0092596da676fedeedaa3bb4da02c104
Jonildo Oliveira <jonildo.oliveira@redeamazonica.com.br>
2022-11-14 12:58:26 -0400

    ajuste chave

--- setup/settings.py
+++ setup/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dq^bh5nhn2-2qdb!3%78-)@!k_#(+v&bnm00dqn-#!3*jkqgj%'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
